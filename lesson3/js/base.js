a49e6604ac50f89ab7defc6b37bff8599462b4e4
mjhaymond <sco97004@byui.edu>
2022-05-04 11:29:57 -0600

    push

---
+++ lesson3/js/base.js
@@ -0,0 +1,7 @@
+let newDate = new Date();
+
+document.getElementById("currentYear").textContent = newDate.getFullYear();
+
+let lastUpdated = document.lastModified;
+
+document.getElementById("updatedDate").textContent = lastUpdated;
