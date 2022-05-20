48ecb3e582f2a80ff9d7ed9af681ddece490e50f
mjhaymond <sco97004@byui.edu>
2022-05-20 17:04:56 -0600

    p

--- bomtop25/js/script.js
+++ bomtop25/js/script.js
@@ -5,22 +5,23 @@
 
       button.addEventListener('click', () => {
         const myItem = input.value;
-        input.value = '';
+        if(myItem != "") {
+         input.value = '';
 
-        const listItem = document.createElement('li');
-        const listText = document.createElement('span');
-        const listBtn = document.createElement('button');
+          const listItem = document.createElement('li');
+          const listText = document.createElement('span');
+          const listBtn = document.createElement('button');
 
-        listItem.appendChild(listText);
-        listText.textContent = myItem;
-        listItem.appendChild(listBtn);
-        listBtn.textContent = 'X';
-        list.appendChild(listItem);
+          listItem.appendChild(listText);
+          listText.textContent = myItem;
+          listItem.appendChild(listBtn);
+          listBtn.textContent = 'X';
+          list.appendChild(listItem);
 
-        listBtn.addEventListener('click', () => {
-          list.removeChild(listItem);
-        });
-
+          listBtn.addEventListener('click', () => {
+            list.removeChild(listItem);
+          });
+        }
         input.focus();
       });
     
