6270ba9ea040a5785e07aa592481f62d8ddfe01b
mjhaymond <sco97004@byui.edu>
2022-05-30 08:15:29 -0600

    push

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,5 +1,7 @@
 // get all imgs with data-scr attribute
-let imagesToLoad = document.querySelectorAll("img[data-src]");
+let imagesToLoad = document.querySelectorAll('img[data-src]')
+
+// This is a function that will replace the src with the data-src
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
@@ -7,25 +9,32 @@ const loadImages = (image) => {
     };
 };
 
+// options that will determine when the image is visible
 const imgOptions = {
     threshold: 1,
     rootMargin: "0px 0px -200px 0px"
 };
 
-if('IntersectionObser' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if(item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
-}, imgOptions);
+// callback function that happens when the image is observed
+const intersectionCallback = (items, observer) => {
+    items.forEach((item) => {
+        if(item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target);
+        }
+    });
+}
 
-
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+// checking if intersectionObserver is supported
+if('IntersectionObserver' in window) {
+    // create the observer
+    const observer = new IntersectionObserver(intersectionCallback, imgOptions);
+    
+    // for each image, observe the image with the observer
+    imagesToLoad.forEach((img) => {
+        observer.observe(img)
+    });
+// if it's not supported just load the images
 } else {
     imagesToLoad.forEach((img) => {
         loadImages(img);
