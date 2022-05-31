d6e38a534eab8a304c5eecdf9a11c694a35d014c
mjhaymond <sco97004@byui.edu>
2022-05-31 13:06:25 -0600

    push

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -37,33 +37,44 @@ close.addEventListener("click", () => {
 
 /**************************************************************************/
 /************************DISCOVER PAGE - LAZY LOAD*************************/
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+// get all imgs with data-scr attribute
+let imagesToLoad = document.querySelectorAll('img[data-src]')
+
+// This is a function that will replace the src with the data-src
 const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src')
+    };
 };
 
+// options that will determine when the image is visible
 const imgOptions = {
-  threshold: 0.5,
-  rootMargin: "0px 0px -250px 0px"
+    threshold: 1,
+    rootMargin: "0px 0px -200px 0px"
 };
 
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if(item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target)
-            }
-        })
-    }, imgOptions);
-
-    imagesToLoad.forEach((img) => {
-        observer.observe(img);
+// callback function that happens when the image is observed
+const intersectionCallback = (items, observer) => {
+    items.forEach((item) => {
+        if(item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target);
+        }
     });
-} else { 
+}
+
+// checking if intersectionObserver is supported
+if('IntersectionObserver' in window) {
+    // create the observer
+    const observer = new IntersectionObserver(intersectionCallback, imgOptions);
+    
+    // for each image, observe the image with the observer
+    imagesToLoad.forEach((img) => {
+        observer.observe(img)
+    });
+// if it's not supported just load the images
+} else {
     imagesToLoad.forEach((img) => {
         loadImages(img);
     });
