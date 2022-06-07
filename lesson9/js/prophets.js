90112bd3259360acf701bb19ae0551609a02cc7f
mjhaymond <sco97004@byui.edu>
2022-06-07 14:13:38 -0600

    push

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,5 +1,4 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-const cards = document.querySelector('.cards');
 
 
 fetch(requestURL)
@@ -8,9 +7,39 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
+
     const prophets = jsonObject['prophets'];
-    prophets.forEach(displayProphets);
+    const cards = document.querySelector('.cards');
+
+    prophets.forEach(prophet =>{
+            // Create elements to add to the document
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let  birthdate = document.createElement('p');
+            let  birthplace = document.createElement('p');
+            let portrait = document.createElement('img');
+          
+            // Change the textContent property of the h2 element to contain the prophet's full name
+            h2.textContent = `${prophet.name} ${prophet.lastname}`;
+            birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+            birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+          
+            // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+            portrait.setAttribute('src', `${prophet.imageurl}`);
+            portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President`);
+            portrait.setAttribute('loading', 'lazy');
+          
+            // Add/append the section(card) with the h2 element
+            card.appendChild(h2);
+            card.appendChild(birthdate);
+            card.appendChild(birthplace);
+            card.appendChild(portrait);
+          
+            // Add/append the existing HTML div with the cards class with the section(card)
+            document.querySelector('div.cards').appendChild(card);
+          
+    });
   });
 
 
-  `Welcome back ${firstName}! You last visited on <date>${lvdate}</date>.`;
+  
