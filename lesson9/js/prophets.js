7979bc9c35c52733ba03b3c8bf0f7918e932d96b
mjhaymond <sco97004@byui.edu>
2022-06-07 12:52:26 -0600

    push

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,16 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
+
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    prophets.forEach(displayProphets);
+  });
+
+
+  `Welcome back ${firstName}! You last visited on <date>${lvdate}</date>.`;
