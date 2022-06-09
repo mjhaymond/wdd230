ef472e80bccbeafbb1b84620183c577d31689f8a
mjhaymond <sco97004@byui.edu>
2022-06-09 10:17:49 -0600

    push

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -50,3 +50,24 @@ fetch(requestURL)
           
     });
   });
+
+  /* --------- buttons ---------- */
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".groups");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
