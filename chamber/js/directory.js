af7884b5c20a945d46bf205c0b76e9b1383a7d30
mjhaymond <sco97004@byui.edu>
2022-06-09 08:30:00 -0600

    push

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -8,43 +8,44 @@ fetch(requestURL)
     console.table(jsonObject);  // temporary checking for valid response and data parsing
 
     const directory = jsonObject['directory'];
-    const cards = document.querySelector('.cards');
+    const groups = document.querySelector('.groups');
 
-    directory.forEach(company =>{
-            // Create elements to add to the document
-           
-            let card = document.createElement('section');
-            let logo = document.createElement('logo');
-            let h2 = document.createElement('h2');
+    directory.forEach(company => {
+            // Create elements to add to the document          
+            let  group = document.createElement('div');
+            let  picture = document.createElement('img');
+            let  companyName = document.createElement('h3');
             let  address = document.createElement('p');
-            let  phone = document.createElement('p');
+            let  phoneNumber = document.createElement('p');
             let  website = document.createElement('p');
-            let  mlevel = document.createElement('p');
-            let picture = document.createElement
+            let  membership = document.createElement('p');
            
-          
-            // Change the textContent property of the h2 element to contain the prophet's full name
-            h2.textContent = `${company.name}`;
-            address.textContent = `company address: ${company.address}`;
-            phone.textContent = `company phone number: ${company.phone}`;
-            website.textContent = `company website: ${company.website}`;
-            mlevel.textContent = `company membership level: ${company.mlevel}`;
-          
+
             // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-            logo.setAttribute('src', `${logo.imageurl}`);
-            logo.setAttribute('alt', `Logo of ${company.name} company logo`);
-            logo.setAttribute('loading', 'lazy');
+            picture.setAttribute('src', "images/" + company.logo);
+            picture.setAttribute('alt', `${company.companyName}`);
+            picture.setAttribute('loading', 'lazy');
+
+          
+            // Change the textContent property of the h3 element to contain the companies full name
+            companyName.textContent = `${company.companyName}`;
+            address.textContent = `${company.address}`;
+            phoneNumber.textContent = `${company.phoneNumber}`;
+            website.textContent = `${company.website}`;
+            membership.textContent = `${company.membership}`;
+          
           
             // Add/append the section(card) with the h2 element
-            card.appendChild(h2);
-            card.appendChild(address);
-            card.appendChild(phone);
-            card.appendChild(website);
-            card.appendChild(mlevel);
+            group.appendChild(companyName);
+            group.appendChild(address);
+            group.appendChild(phoneNumber);
+            group.appendChild(website);
+            group.appendChild(membership);
+            group.appendChild(picture);
           
             // Add/append the existing HTML div with the cards class with the section(card)
             //document.querySelector('div.cards').appendChild(card);
-            cards.append(cards);
+            groups.append(group);
           
     });
   });
