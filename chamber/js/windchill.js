db05d94ee262307941461fb383da4add8849564f
mjhaymond <sco97004@byui.edu>
2022-06-24 12:51:44 -0600

    push

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,21 +1,21 @@
 const currentTemp = document.querySelector("#currtemp");
 const weatherIcon = document.querySelector("#weathericon");
 const captionDesc = document.querySelector("figcaption");
+const windspeed = document.querySelector("#windspeed");
+const windchill = document.querySelector("#windchill");
 
 const url =
   "//api.openweathermap.org/data/2.5/weather?q=Lehi&units=imperial&appid=41f047a75e03e2230bb3b7afc89cc6fd";
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    //console.log(data);//
 
-    const windspeed = document.querySelector("#windspeed");
-
+  
     currentTemp.innerHTML = `${data.main.temp.toFixed(0)}`;
     windspeed.innerHTML = `${data.wind.speed}`;
 
-    const windchill = document.querySelector("#windchill");
-    if ((currentTemp <= 50) && (windspeed > 3)) {
+    
+    if ((data.main.temp <= 50) && (data.wind.speed > 3)) {
     const chill = 35.74 + (0.6215 * currentTemp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * currentTemp * Math.pow(windspeed, .16));
     windchill.innerHTML = chill.toFixed(1);
     } else {
