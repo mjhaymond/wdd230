cd2f0df8dea9e0c053979e237cb36157a0babcee
mjhaymond <sco97004@byui.edu>
2022-07-02 16:37:57 -0600

    push

--- templeinn/js/temples.js
+++ templeinn/js/temples.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://mjhaymond.github.io/wdd230/chamber/json/data.json';
+const requestURL = 'https://mjhaymond.github.io/wdd230/templeinn/json/temples.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -7,41 +7,50 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
 
-    const directory = jsonObject['directory'];
-    const groups = document.querySelector('.groups');
+    const temples = jsonObject['temples'];
+    const groups = document.querySelector('.templeCards');
 
-    directory.forEach(company => {
+    directory.forEach(temple => {
             // Create elements to add to the document  
             let  picture = document.createElement('img');        
             let  group = document.createElement('div');
-            let  companyName = document.createElement('h2');
+            let  templeName = document.createElement('h2');
             let  address = document.createElement('p');
             let  phoneNumber = document.createElement('p');
-            let  website = document.createElement('p');
-            let  membership = document.createElement('p');
+            let  email = document.createElement('p');
+            let  services = document.createElement('p');
+            let  announcement = document.createElement('p');
+            let  groundbreaking = document.createElement('p');
+            let  dedication = document.createElement('p');
            
 
             // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-            picture.setAttribute('src', "images/" + company.logo);
-            picture.setAttribute('alt', `${company.companyName}`);
+            picture.setAttribute('src', "images/" + temple.picture);
+            picture.setAttribute('alt', `${temple.templeName}`);
             picture.setAttribute('loading', 'lazy');
 
           
             // Change the textContent property of the h3 element to contain the companies full name
-            companyName.textContent = `${company.companyName}`;
-            address.textContent = `${company.address}`;
-            phoneNumber.textContent = `${company.phoneNumber}`;
-            website.textContent = `${company.website}`;
-            membership.textContent = `${company.membership}`;
+            templeName.textContent = `${temple.templeName}`;
+            address.textContent = `${temple.address}`;
+            phoneNumber.textContent = `${temple.phoneNumber}`;
+            email.textContent = `${temple.email}`;
+            services.textContent = `${temple.services}`;
+            announcement.textContent = `${temple.announcement}`;
+            groundbreaking.textContent = `${temple.groundbreaking}`;
+            dedication.textContent = `${temple.dedication}`;
           
           
             // Add/append the section(card) with the h2 element
             group.appendChild(picture);
-            group.appendChild(companyName);
+            group.appendChild(templeName);
             group.appendChild(address);
             group.appendChild(phoneNumber);
-            group.appendChild(website);
-            group.appendChild(membership);
+            group.appendChild(email);
+            group.appendChild(services);
+            group.appendChild(announcement);
+            group.appendChild(groundbreaking);
+            group.appendChild(dedication);
             
           
             // Add/append the existing HTML div with the cards class with the section(card)
@@ -71,3 +80,4 @@ function showList() {
 	display.classList.add("list");
 	display.classList.remove("grid");
 }
+
