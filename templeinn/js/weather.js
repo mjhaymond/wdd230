ed6ef0b4f3852e07b684c7e2b1b40cecf5faccd4
mjhaymond <sco97004@byui.edu>
2022-07-16 11:45:23 -0600

    push

--- templeinn/js/weather.js
+++ templeinn/js/weather.js
@@ -1,81 +1,44 @@
-const apiURL_forecast =
-  "//api.openweathermap.org/data/2.5/weather?q=Lehi&units=imperial&appid=511d657ffd28851acb7688ddca57d746";
+// Current Weather for Summary
+//const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&APPID=511d657ffd28851acb7688ddca57d746";
 
-fetch(apiURL_forecast) 
-  .then((response) => response.json())
-  .then((jsObject) => )
-    var weekday = now Array(7);
-    weekday[0] = "Sun";
-    weekday[1] = "Mon";
-    weekday[2] = "Tue";
-    weekday[3] = "Wed";
-    weekday[4] = "Thu";
-    weekday[5] = "Fri";
-    weekday[6] = "Sat";
+//const apiURL = "https://api.openweathermap.org/data/2.5/weather?zip=84045,us&appid=511d657ffd28851acb7688ddca57d746";
 
-    const data =jsObject.list.filter((element)=>
-    element.dt_txt.includes("19:00:00")
-    );
-
-    const dayOfWeek = document.getElementsByClassName("day");
-    const weatherIcon = document.getElementsByClassName("w-icon");
-    const dayTemp = document.getElementsByClassName("d-temp");
-
-    for (var i = 0; i < data.length; i++) (
-    var d = now Date(date[1].dt_txt);
-    dayOfWeek[i].textContent=weekday[d.getDay()];
-
-    const imagesrc = 
-    "https://openweathermap.org/img/w/" + data[1].weather[0].icon + ".png";
-    const description = data[0].description;
-    weatherIcon[i].setAttribute("src", imagesrc);
-    weatherIcon[i].setAttribute("alt", description);
-
-    dayTemp [i].inner = Math.round(data[i].main.temp) + " &#176;F"
-    )
-    ];
-
-
-
-
-
-
-
-
-
-/*
-// Current Weather for Weather Summary
-const currentTemp = document.querySelector("#currtemp");
-const weatherIcon = document.querySelector("#weathericon");
-const captionDesc = document.querySelector("figcaption");
-const windspeed = document.querySelector("#windspeed");
-const windchill = document.querySelector("#windchill");
-
-const url =
-  "//api.openweathermap.org/data/2.5/weather?q=Lehi&units=imperial&appid=511d657ffd28851acb7688ddca57d746";
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-
+const url = "https://api.openweathermap.org/data/3.0/onecall?lat=40.3916&lon=-111.8508&exclude=minutely,hourly&units=imperial&appid=511d657ffd28851acb7688ddca57d746"
   
-    currentTemp.innerHTML = `${data.main.temp.toFixed(0)}`;
-    windspeed.innerHTML = `${data.wind.speed}`;
-
-    
-    if ((data.main.temp <= 50) && (data.wind.speed > 3)) {
-    const chill = 35.74 + (0.6215 * currentTemp) - (35.75 * Math.pow(windspeed, .16)) + (0.4275 * currentTemp * Math.pow(windspeed, .16));
-    windchill.innerHTML = chill.toFixed(1);
-    } else {
-    windchill.innerHTML = "N/A";
-    }
-
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    const desc = data.weather[0].description;
-
-    weatherIcon.setAttribute("src", iconsrc);
-    weatherIcon.setAttribute("alt", desc);
-    captionDesc.innerHTML = desc;
+fetch(apiURL)
+.then((response) => response.json())
+.then((town) => {
+  //console.log(town);
+  let description = town.weather[0].description;
+  document.getElementById('currently').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
+  document.getElementById('temperature').innerHTML = Math.round(town.main.temp);
+  document.getElementById('humidity').innerHTML = Math.round(town.wind.speed);
+});
 
 
-  });
-  */
+
+// 3 day forecast
+
+const apiURL_forecast = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&APPID=511d657ffd28851acb7688ddca57d746"
+
+fetch(apiURL_forecast)
+  .then(response => response.json())
+  .then((jsObject) => {
+      console.log(jsObject);
+      const forecastData = jsObject.list.filter((element)=>element.dt_txt.includes('18:00:00'));
+
+console.log(forecastData);
+
+const weekdays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
+  let day = 0;
+forecastData.forEach(forecast => {
+  let x = new Date(forecast.dt_txt);
+  document.getElementById('temp'+(day+1)).textContent = Math.round(forecast.main.temp) + ' °F';
+  document.getElementById('img'+(day+1)).src = "https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png";
+  document.getElementById('img'+(day+1)).alt = forecast.weather[0].description
+document.getElementById('day'+(day+1)).textContent = weekdays[x.getDay()];
+day++;	  
+});
+});
+
